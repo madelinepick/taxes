e634104f3b8d44662bac5448f1e10cce21de4c37
Madeline Pickering <madelinepickering@gmail.com>
2016-02-22 09:37:29 -0700

    fixed rounding error and descriptions

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ module.exports = {
     var tax = 0;
     if ( income <=10 ) {
       tax = income*0.1;
-      return tax;
+      return Math.round(tax*100)/100;
     }
     else if ( income <=20 ) {
       tax = (1+((income-10)*0.07));
-      return tax;
+      return Math.round(tax*100)/100;
     }
     else if ( income <=30 ) {
       tax = (1.7+ ((income-20)*0.05));
-      return tax;
+      return Math.round(tax*100)/100;
     }
     else {
      tax = (2.2+ ((income-30)*0.03));
-     return tax;
+     return Math.round(tax*100)/100;
    }
  }
 }
--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
 var code = require('../main.js')
 var expect = require('chai').expect
 
-describe('leapYear', function(){
-  it('first ten', function(){
+describe('taxes', function(){
+  it('first ten is 10%', function(){
     expect(code.taxCalculator(1)).to.equal(0.1);
     expect(code.taxCalculator(10)).to.equal(1);
   })
-  it('second ten', function(){
+  it('second ten is 7%', function(){
     expect(code.taxCalculator(15)).to.equal(1.35);
     expect(code.taxCalculator(20)).to.equal(1.7);
   })
-  it('third ten', function(){
+  it('third ten is 5%', function(){
     expect(code.taxCalculator(25)).to.equal(1.95);
     expect(code.taxCalculator(30)).to.equal(2.2);
   })
-  it('the rest', function(){
+  it('the rest is 3%', function(){
     expect(code.taxCalculator(40)).to.equal(2.5)
   })
 })
